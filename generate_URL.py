ec9638872510a628f123c65387a537f36dea7380
gaurikatyagi <gtyagi@dons.usfca.edu>
2016-05-29 10:34:54 -0700

    Add docstrings for explanation

--- generate_URL.py
+++ generate_URL.py
@@ -1,4 +1,5 @@
-#This program is used to generate the urls and help the user understand how the urls will look to the user when accessing this application on a website
+#This program is used to generate the urls and help the user understand how the urls will look to the user when
+# accessing this application on a website
 
 from flask import Flask, url_for
 
@@ -17,6 +18,13 @@ def profile(username):
     return "Welcome %s"%username
 
 with app.test_request_context():
+    """
+    test_request_context method tells flask to behave as though it is handling a request even though we are interacting
+    with the python terminal by calling the program without activating the server.
+    We see the urls like this and use them like this when we want to use them in aprogram and do not want to hardcode
+    them, allowing us to change the URLs in one go, rather than having to change them evrywhere in the code.
+
+    """
     print url_for("index")
     print url_for("login")
     print url_for("login", next = "/")
