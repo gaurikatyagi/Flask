ecd940be3c776fb18c7f980c0130350bf2442f93
gaurikatyagi <gtyagi@dons.usfca.edu>
2016-05-29 20:12:34 -0700

    Add database configuration

--- micro_blogging/flaskr.py
+++ micro_blogging/flaskr.py
@@ -1,8 +1,21 @@
 import sqlite3
 from flask import Flask, request, session, redirect, g, url_for, abort, render_template, flash
 
-database = "/tmp/microblog.db"
-debug = True
-secret_key = "development key"
-username = "admin"
-password = "defaultpass"
+#database configuration
+DATABASE = "/tmp/microblog.db"
+DEBUG = True
+SECRET_KEY = "development key"
+USERNAME = "admin"
+PASSWORD = "defaultpass"
+
+#main application
+app = Flask(__name__)
+app.config.from_object(__name__)
+##from_object() will look at the given object (if it's a string it will import it) and then
+##look for all uppercase variables defined there
+
+def connect_db():
+    return sqlite3.connect(app.config["DATABASE"])
+
+if __name__ == "__main__":
+    app.run()
