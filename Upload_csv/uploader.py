0b3a2f04da46e1c53299f2605b82e31500d57f61
gaurikatyagi <gtyagi@dons.usfca.edu>
2016-06-01 07:49:09 -0700

    Include login/logout for upload

--- Upload_csv/uploader.py
+++ Upload_csv/uploader.py
@@ -1,21 +1,61 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, flash, session, redirect
+import os
 
 app = Flask(__name__)
+ALLOWED_EXTENSIONS = set(["xlxs", "xls", "csv"])
+USERNAME = "admin"
+PASSWORD = "gt"
+SECRET_KEY = "development key"
 
+app.config.from_object(__name__)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def upload_file():
-    return render_template("file_upload.html")
+    return render_template("home_screen.html")
 
 
 @app.route('/uploader', methods=['GET', 'POST'])
 def uploader():
     if request.method == 'POST':
-        f = request.files['file']
-        f.save(f.filename)
-        return 'file uploaded successfully'
-    else:
-        return 'Try again'
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return render_template("file_upload.html")
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return render_template("file_upload.html")
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash("File Uploaded")
+            return render_template("file_upload.html")
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        if request.form["username"] != app.config["USERNAME"]:
+            error = "Invalid Username"
+        elif request.form["password"] != app.config["PASSWORD"]:
+            error = "Invalid Password"
+        else:
+            session["logged_in"] = True
+            flash(message = "You are now logged in")
+            return render_template("file_upload.html")
+    return render_template("login.html", error = error)
+
+@app.route("/logout")
+def logout():
+    session.pop("logged_in", None)
+    flash(message = "You're now logged out")
+    return redirect(url_for("upload_file"))
 
 
 if __name__ == '__main__':
