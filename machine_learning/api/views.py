bb14fff69f9fffde931e095f109f30f627635478
gaurikatyagi <gtyagi@dons.usfca.edu>
2016-06-08 19:33:17 -0700

    Add pca and plotting

--- machine_learning/api/views.py
+++ machine_learning/api/views.py
@@ -3,6 +3,12 @@ import os
 import pandas as pd
 import json
 from flask import jsonify
+from flask import render_template
+from sklearn import preprocessing
+from sklearn import decomposition
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
 
 def get_data():
     f_name = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data", "breast-cancer-wisconsin.csv")
@@ -12,9 +18,46 @@ def get_data():
     return df.dropna()
 
 
+
 @app.route("/")
 def index():
-    return "Hello, I'm an API!"
+    df = get_data()
+    X = df.ix[:, (df.columns != "class") & (df.columns != "code")].as_matrix()
+    y = df.ix[:, df.columns == "clas"].as_matrix()
+
+    #scale data
+    scaler = preprocessing.StandardScaler().fit(X)
+    scaled = scaler.transform(X)
+
+    #PCA
+    pcomp = decomposition.PCA(n_components = 2)
+    pcomp.fit(scaled)
+    components = pcomp.transform(scaled)
+    var = pcomp.explained_variance_ratio_.sum()
+
+    #Kmeans
+    model = KMeans(init = "k-means++", n_clusters = 2) #K-means++ ensures that the initial centroids which are chosen
+    # are spaced out far apart
+    model.fit(components)
+    plt.scatter(x = components[:, 0], y = components[:, 1], c = model.labels_)
+    centers = plt.plot(
+        [model.cluster_centers_[0, 0], model.cluster_centers_[1, 0]],
+        [model.cluster_centers_[1, 0], model.cluster_centers_[1, 1]],
+        "kx", c = "Green"
+    )
+
+    #Increase size of center points
+    plt.setp(centers, ms = 11.0) #increases size
+    plt.setp(centers, mew = 1.8)
+
+    #Plot axes adjustments
+    axes = plt.gca()
+    axes.set_xlim([-7.5, 3])
+    axes.set_ylim([-2, 5])
+    plt.xlabel("PC1")
+    plt.ylabel("PC2")
+    plt.title("Clustering of PCs ({:.2f}% Var. Explained".format(var*100))
+    return render_template("index.html")
 
 @app.route("/head")
 def head():
